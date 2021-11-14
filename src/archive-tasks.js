6860e4c2ae76d27f13512d4b8f0468e9345d9b91
James Sinclair <jsinclair2@atlassian.com>
2021-11-14 14:49:03 +1100

    Update examples to match article

--- src/archive-tasks.js
+++ src/archive-tasks.js
@@ -1,8 +1,18 @@
 const ONE_MINUTE = 60000;
 
+// Take a function and transform it so that it returns the boolean
+// negation.
+const not = f => x => !f(x);
+
+// Take the current time and a task, and determine if this is an
+// old task that should be archived.
+const isOldTask = currentTime => task => {
+    return task.completed !== undefined && currentTime - task.completed > ONE_MINUTE;
+};
+
 const moveOldTasksToArchive = ({active, archive}, currentTime) => ({
-    active: active.filter(({completed}) => !completed || currentTime - completed < ONE_MINUTE),
-    archive: active.filter(({completed}) => currentTime - completed >= ONE_MINUTE).concat(archive),
+    active: active.filter(not(isOldTask(currentTime))),
+    archive: active.filter(isOldTask(currentTime)).concat(archive),
 });
 
 export default moveOldTasksToArchive;
--- src/archive-tasks.test.js
+++ src/archive-tasks.test.js
@@ -1,63 +1,148 @@
-// import * as fc from "fast-check";
+import * as fc from 'fast-check';
 import moveOldTasksToArchive from './archive-tasks';
 
-// const genTodo = () =>
-//   fc.tuple(fc.date(), fc.nat()).chain((dt, offset) =>
-//     fc.record(
-//       {
-//         created: fc.constant(dt.valueOf()),
-//         completed: fc.constant(dt.valueOf() + offset),
-//         title: fc.string()
-//       },
-//       { requiredKeys: ["created", "title"] }
-//     )
-//   );
+// Some date constants to make life easier. We're using timestamps
+// rather than date objects just to keep the maths simple.
+const START = 1636521855000;
+const ONE_MINUTE = 60000;
+const ONE_HOUR = 60 * ONE_MINUTE;
 
-// const genTaskState = () =>
-//   fc.record({
-//     active: fc.array(genTodo),
-//     archive: fc.array(genTodo)
-//   });
+// We create some example data. All tasks have, at minimum,
+// both a created date and a title. The completed time is optional.
+// A task that has a missing or undefined completed field is not
+// yet done.
+const newTask = {
+    created: START - ONE_MINUTE,
+    title: 'A mighty task of spectacular derring-do',
+    completed: START,
+};
 
+// We intend to pass START as our reference time. So we make an
+// old task that is was completed 59 minutes ago.
+const oldCompletedTask = {
+    created: START - ONE_HOUR,
+    completed: START - ONE_HOUR + ONE_MINUTE,
+    title: 'should be archived',
+};
+
+// This is our basic input. We have an array of 'active' tasks, and
+// an array of 'archive' tasks. The active list has one task we
+// expect to stay in the active list, and one we expect to move.
 const basicInput = {
-    active: [
-        {created: 1636521855000, title: 'some todo'},
-        {
-            created: 1636518194999,
-            completed: 1636521794999,
-            title: 'should be archived',
-        },
-    ],
+    active: [newTask, oldCompletedTask],
     archive: [],
 };
 
+// After we run our archive function we expect the following
+// output:
 const expectedBasic = {
-    active: [{created: 1636521855000, title: 'some todo'}],
-    archive: [
-        {
-            created: 1636518194999,
-            completed: 1636521794999,
-            title: 'should be archived',
-        },
-    ],
+    active: [newTask],
+    archive: [oldCompletedTask],
+};
+
+// We should test the edge case for when the arrays are empty.
+const emptyInput = {active: [], archive: []};
+
+// And we'd also like to test the case where there's something
+// already in the archive. So we'll create another old task…
+const oldAbandonedTask = {
+    created: START - ONE_HOUR,
+    title: 'Abandoned, not completed',
+};
+
+// …and put the old task into the archive to create a new input.
+const populatedArchive = {
+    active: [oldCompletedTask],
+    archive: [oldAbandonedTask],
+};
+
+// This is the expected output for the case where the archive
+// already has something in it.
+const expectedPopulated = {
+    active: [],
+    archive: [oldCompletedTask, oldAbandonedTask],
 };
 
 describe.each`
-    description        | input         | date             | expected
+    description            | input               | date     | expected
 -----------------------------------------------------------------------------
-    ${'Basic example'} | ${basicInput} | ${1636521855000} | ${expectedBasic}
+    ${'Basic example'}     | ${basicInput}       | ${START} | ${expectedBasic}
+    ${'Empty arrays'}      | ${emptyInput}       | ${START} | ${emptyInput}
+    ${'Populated archive'} | ${populatedArchive} | ${START} | ${expectedPopulated}
 `('$description', ({input, date, expected}) => {
-    return expect(moveOldTasksToArchive(input, date)).toEqual(expected);
+    test(`Given a sample state and date,
+          when we run moveOldTasksToArchive(),
+          it should return the expected output`, () => {
+        expect(moveOldTasksToArchive(input, date)).toEqual(expected);
+    });
 });
 
-// describe("moveOldTasksToArchive()", () => {
-//   test(
-//     "Given a task state s, and date dt, after calling, no completed todos in s.active should be older than 60 seconds before date dt"
-//   );
-//   test(
-//     "For any task state s, and date dt, the total number of tasks should not change"
-//   );
-//   test(
-//     "For any task state s, and date dt, after running, there should be no new tasks in s.archive that did not come from the original active list"
-//   );
-// });
+// Property-based tests
+// ------------------------------------------------------------------------------------------------
+
+// Generate a to-do item.
+const genTodo = () => {
+    return fc
+        .record(
+            {
+                created: fc.date().map(d => d.getTime()),
+                title: fc.string(),
+                offset: fc.nat(),
+            },
+            {requiredKeys: ['created', 'title']}
+        )
+        .map(({created, title, offset}) => ({
+            created,
+            title,
+            completed: offset !== undefined ? created + offset : undefined,
+        }));
+};
+
+// Generate a task state.
+const genTaskState = () =>
+    fc.record({
+        active: fc.array(genTodo()),
+        archive: fc.array(genTodo()),
+    });
+
+describe('moveOldTasksToArchive()', () => {
+    test(`GIVEN ANY valid task state and date
+        WHEN we run moveOldTasksToArchive()
+        THEN the total number of tasks SHOULD ALWAYS stay the same`, () => {
+        const lengthProperty = fc.property(genTaskState(), fc.date(), (s, dt) => {
+            const newState = moveOldTasksToArchive(s, dt.getTime());
+            const actualLength = newState.active.length + newState.archive.length;
+            const expectedLength = s.active.length + s.archive.length;
+            expect(actualLength).toBe(expectedLength);
+        });
+        fc.assert(lengthProperty);
+    });
+
+    test(`GIVEN ANY valid task and date
+        WHEN we run moveOldTasksToArchive()
+        THEN all the tasks in .active SHOULD ALWAYS be either 
+            incomplete, or, completed less than 60 seconds
+            before the date`, () => {
+        const allActiveRecentProperty = fc.property(genTaskState(), fc.date(), (s, dt) => {
+            const newState = moveOldTasksToArchive(s, dt.getTime());
+            expect(
+                newState.active.some(
+                    ({completed}) => completed !== undefined && dt - completed > ONE_MINUTE
+                )
+            ).toBe(false);
+        });
+        fc.assert(allActiveRecentProperty);
+    });
+
+    test(`GIVEN ANY valid task and date
+        WHEN we run moveOldTasksToArchive()
+        THEN there SHOULD NEVER be any tasks in the archive that weren't in the original state`, () => {
+        const noNewTasksProperty = fc.property(genTaskState(), fc.date(), (s, dt) => {
+            const {archive} = moveOldTasksToArchive(s, dt.getTime());
+            expect(archive.every(task => s.archive.includes(task) || s.active.includes(task))).toBe(
+                true
+            );
+        });
+        fc.assert(noNewTasksProperty);
+    });
+});
