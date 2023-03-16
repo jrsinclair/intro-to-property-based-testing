221baf24d49afb9bdf1688a8620565f7a5ade3e4
James Sinclair <jsinclair2@atlassian.com>
2023-03-16 11:53:29 +1100

    Add TS support and begin rearranging things

---
+++ src/ex01/index.solution.ts
@@ -0,0 +1,18 @@
+const moveOldTasksToArchive = (tasks: unknown, start: number) => ({
+    active: [
+        {
+            created: 1636521795000,
+            title: 'A mighty task of spectacular derring-do',
+            completed: 1636521855000,
+        },
+    ],
+    archive: [
+        {
+            created: 1636518255000,
+            completed: 1636518315000,
+            title: 'A task that should be archived',
+        },
+    ],
+});
+
+export default moveOldTasksToArchive;
---
+++ src/ex01/index.ts
@@ -0,0 +1,7 @@
+import {TasksState} from '../types';
+
+const moveOldTasksToArchive = (tasks: TasksState, start: number) => ({
+    // Your solution goes here. Write only enough code to make the test pass. No more.
+});
+
+export default moveOldTasksToArchive;
